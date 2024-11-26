1a5cf2ecf9ff8e6e4a9e0153ac3b7f8d06479e3f
Giovanni <158181635+giovannig10@users.noreply.github.com>
2024-11-26 11:12:00 -0300

    create inserts

---
+++ giovanni.sql
@@ -0,0 +1,11 @@
+INSERT INTO questoes (enunciado, resposta_a, resposta_b, resposta_c, resposta_certa) VALUES
+(‘1. Qual é o maior continente do mundo?’, ‘África’, ‘Ásia’, ‘América do Norte’, ‘Ásia’),
+(‘2. Qual é a capital do Brasil?’, ‘São Paulo’, ‘Brasília’, ‘Rio de Janeiro’, ‘Brasília’),
+(‘3. O que é o clima equatorial?’, ‘Quente e seco’, ‘Frio e úmido’, ‘com chuvas constantes’, ‘com chuvas consatantes’),
+(‘4. Qual é o principal bioma encontrado na Amazônia?’, ‘Cerrado’, ‘Floresta tropical’, ‘Savana’, ‘Cerrado’),
+(‘5. O que caracteriza a desertificação?’, ‘Aumento da umidade em regiões áridas’, ‘Transformação de terras férteis em desertos’, ‘Crescimento de florestas em áreas urbanas’, ‘Transformação de terras férteis em desertos’),
+(‘6. Qual é a principal fonte de energia utilizada no Brasil?’, ‘Energia solar’, ‘Energia hidrelétrica’, ‘Energia eólica’, ‘Energia hifrelétrica’),
+(‘7. Qual das seguintes opções é um estado da região Norte do Brasil?’, ‘Bahia’, ‘Maranhão’, ‘Acre’, ‘Maranhão’),
+(‘8. Qual é o principal rio da América do Sul?’, ‘Rio Paraná’, ‘Rio Amazonas’, ‘Rio São Francisco’, ‘Rio Amazonas’),
+(‘9. O que são zonas de convergência intertropical (ZCIT)?’, ‘Regiões de alta pressão atmosférica’, ‘Áreas onde ventos alísios se encontram gerando chuvas’, ‘Regiões polares com baixas temperaturas’, ‘Áreas onde ventos alísios se encontram, gerando chuvas’),
+(‘10. Qual é o efeito causado pela poluição atmosférica nas cidades?’, ‘Melhoria da qualidade do ar’, ‘Formação de ilhas de calor’, ‘Aumento da biodiversidade’, ‘Formação de ilhas de calor’);
